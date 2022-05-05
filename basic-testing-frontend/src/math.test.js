0449d45dfc089f701b630df550f1501fb465f6b3
Mario Lazzari <mario.lazzari@gmail.com>
2022-05-05 20:44:05 +0200

    Done lesson 21

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -10,3 +10,22 @@ it("should add all numbers of an array", () => {
   const expected = numbers.reduce((acc, curr) => acc + curr, 0);
   expect(result).toBe(expected);
 });
+
+it("should yeald NaN if at least one non number is present", () => {
+  // arrange
+  const numbers = [1, 2, "a", 4, 5];
+  // act
+  const result = add(numbers);
+  // assert
+  expect(result).toBeNaN();
+});
+
+it("should add all string number", () => {
+  // arrange
+  const numbers = ["1", "2", "3", "4", "5"];
+  // act
+  const result = add(numbers);
+  // assert
+  const expected = numbers.reduce((acc, curr) => +acc + +curr, 0);
+  expect(result).toBe(expected);
+});
