f3ec850ab39bbb09248acdf4d9e894e4f217b906
Mario Lazzari <mario.lazzari@gmail.com>
2022-05-05 20:15:25 +0200

    Done lesson 20

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,4 +1,12 @@
-import { it } from "vitest";
+import { it, expect } from "vitest";
 import { add } from "./math";
 
-it("should add all numbers of an array", () => {});
+it("should add all numbers of an array", () => {
+  // arrange
+  const numbers = [1, 2, 3, 4, 5];
+  // act
+  const result = add(numbers);
+  // assert
+  const expected = numbers.reduce((acc, curr) => acc + curr, 0);
+  expect(result).toBe(expected);
+});
