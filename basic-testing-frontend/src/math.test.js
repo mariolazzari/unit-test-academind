d79041b19fece61b1a572bc59d8e6ac091a02611
Mario Lazzari <mario.lazzari@gmail.com>
2022-05-06 20:31:54 +0200

    Done lesson 24

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -29,3 +29,32 @@ it("should add all string number", () => {
   const expected = numbers.reduce((acc, curr) => +acc + +curr, 0);
   expect(result).toBe(expected);
 });
+
+it("should get 0 in empty array", () => {
+  // arrange
+  const numbers = [];
+  // act
+  const result = add(numbers);
+  // assert
+  expect(result).toBe(0);
+});
+
+it("should throw error if no argument is passed", () => {
+  // arrange
+  const numbers = undefined;
+  // act
+  const result = () => add(numbers);
+  // assert
+  expect(result).toThrow();
+  //expect(result).not.toThrow();
+});
+
+it("should throw an error if multiple arguments are passed", () => {
+  // arrange
+  const arg1 = 1;
+  const arg2 = 2;
+  // act
+  const result = () => add(numbers(arg1, arg2));
+  // assert
+  expect(result).toThrow();
+});
