a685711fda8f021223f05decf2b55562e52d3f5c
Mario Lazzari <mario.lazzari@gmail.com>
2022-05-11 21:42:37 +0200

    Done lesson 52

--- async-example/async/async.test.js
+++ async-example/async/async.test.js
@@ -1,18 +1,48 @@
-import { it, expect } from "vitest";
-import { generateToken } from "./async-example.js";
+import { describe, it, expect } from "vitest";
+import { generateToken, generateTokenPromise } from "./async-example.js";
 
 // Callbacks: done -> once test is done! (use try catch to avoid timeout)
-it("should generate a token", done => {
-  // arrange
-  const mail = "mario.lazzari@gmail.com";
-  // act
-  generateToken(mail, (_err, token) => {
-    try {
-      // assert
-      expect(token).toBeDefined();
-      done();
-    } catch (err) {
-      done(err);
-    }
+
+describe("generateToken()", () => {
+  it("should generate a token", done => {
+    // arrange
+    const mail = "mario.lazzari@gmail.com";
+    // act
+    generateToken(mail, (_err, token) => {
+      try {
+        // assert
+        expect(token).toBeDefined();
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
   });
+});
+
+describe("generateTokenPromise()", () => {
+  it("should generate a token", () => {
+    // arrange
+    const mail = "mario.lazzar@gmail.com";
+    // act
+    expect(generateTokenPromise(mail)).resolves.toBeDefined();
+  });
+
+  it("should generate a token (async)", async () => {
+    // arrange
+    const mail = "mario.lazzari@gmail";
+    // act
+    const res = await expect(generateTokenPromise(mail));
+    // assert
+    expect(res).toBeDefined();
+  });
+
+  /*
+  it("should reject if mail is not valid", () => {
+    // arrange
+    const mail = null;
+    // act
+    expect(generateTokenPromise(mail)).rejects.toThrow();
+  });
+  */
 });
