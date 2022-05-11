8f7c32d1f5636141c5b95fe8b0b3b9555f1856f3
Mario Lazzari <mario.lazzari@italcoeng.com>
2022-05-11 15:15:30 +0200

    Done lesson 47

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,23 +1,45 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, it, expect } from "vitest";
+import { transformToNumber, cleanNumbers } from "./numbers";
 
-it("shout convert string to number", () => {
-  // arrange
-  const str = "1";
-  // act
-  const result = transformToNumber(str);
-  // assert
-  expect(result).toBeTypeOf("number");
+describe("transformToNumber", () => {
+  it("shout convert string to number", () => {
+    // arrange
+    const str = "1";
+    // act
+    const result = transformToNumber(str);
+    // assert
+    expect(result).toBeTypeOf("number");
+  });
+
+  it("it should return NaN if non numeric argument passed", () => {
+    // arrange
+    const arg1 = "arg";
+    const arg2 = {};
+    // act
+    const res1 = transformToNumber(arg1);
+    const res2 = transformToNumber(arg2);
+    // assert
+    expect(res1).toBeNaN();
+    expect(res2).toBeNaN();
+  });
 });
 
-it("it should return NaN if non numeric argument passed", () => {
-  // arrange
-  const arg1 = "arg";
-  const arg2 = {};
-  // act
-  const res1 = transformToNumber(arg1);
-  const res2 = transformToNumber(arg2);
-  // assert
-  expect(res1).toBeNaN();
-  expect(res2).toBeNaN();
+describe("cleanNumbers()", () => {
+  it("should return an array of numbers from an array of strings", () => {
+    // arrange
+    const arr = ["1", "2", "3"];
+    // act
+    const result = cleanNumbers(arr);
+    // assert
+    expect(result[0]).toBeTypeOf("number");
+  });
+
+  it("should throws an error on empty string", () => {
+    // arrange
+    const arr = ["1", "", "3"];
+    // act
+    const result = () => cleanNumbers(arr);
+    // assert
+    expect(result).toThrow();
+  });
 });
